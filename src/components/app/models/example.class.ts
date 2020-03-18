220b9ec9853823f9b79f14f8a1d9c095917d4b42
Azarattum <azarattum@pm.me>
2020-03-19 00:51:26 +0700

    Added model example

---
+++ src/components/app/models/example.class.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents some example model
+ */
+export default class Example {
+	// eslint-disable-next-line @typescript-eslint/no-useless-constructor
+	public constructor() {
+		///Model class construction goes here
+	}
+}

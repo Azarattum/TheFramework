20f7bf7f6978441ef318075ebec3b69cea267e44
Azarattum <azarattum@pm.me>
2020-03-19 01:26:00 +0700

    Added hash service & tabs controller

---
+++ src/components/app/controllers/tabs.controller.ts
@@ -0,0 +1,64 @@
+import View from "../../common/view.abstract";
+import Controller from "../../common/controller.abstract";
+
+/**
+ * Service for controlling application views as tabs
+ */
+export default class Tabs extends Controller<"tabchanged">() {
+	private tabs: View[] = [];
+
+	/**
+	 * Creates template controller
+	 */
+	public constructor(name: string = "") {
+		super("Tabs" + name);
+	}
+
+	/**
+	 * Initializes tabs service
+	 * @param tabs Tabs views to control
+	 */
+	public initialize(tabs: View[]): void {
+		this.tabs = tabs;
+
+		this.expose("change");
+	}
+
+	/**
+	 * Changes current opened tab
+	 * @param tabName The open of tab to open
+	 */
+	public change(tabName: string): void {
+		let name;
+		//View tabs
+		if (this.tabs && this.tabs.length > 0) {
+			const selected = this.tabs.find(
+				tab => tab.name.toLowerCase() == tabName.toLowerCase()
+			);
+			if (!selected) {
+				throw new Error(`Tab ${tabName} does not exist!`);
+			}
+
+			name = selected.name;
+			this.tabs.forEach(tab => tab.toggle(false));
+			selected.toggle(true);
+		}
+		//Element tabs
+		else {
+			const selected = this.container.querySelector(
+				`[tab="${tabName.toLowerCase()}"]`
+			);
+			if (!selected) {
+				throw new Error(`Tab ${tabName} does not exist!`);
+			}
+
+			this.container.querySelectorAll("[tab]").forEach(x => {
+				(x as HTMLElement).style.display = "none";
+			});
+			(selected as HTMLElement).style.display = "block";
+			name = selected.getAttribute("name");
+		}
+
+		this.emit("tabchanged", name);
+	}
+}

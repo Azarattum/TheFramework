29f9a147d453998963c860f4a8c4387d6dfdaf47
Azarattum <azarattum@pm.me>
2020-11-11 20:23:49 +0700

    Improved element decorators. Added bind decorator

--- src/components/common/controller.abstract.ts
+++ src/components/common/controller.abstract.ts
@@ -142,14 +142,14 @@ export default function Controller<T extends string>() {
 						(object.__origin ? object.__origin + "." : "") +
 						property;
 
-					this.binding.set(path, value);
+					this.binding?.set(path, value);
 
 					return true;
 				}
 			};
 
 			//Assign root object to data
-			const data: any = this.binding.get();
+			const data: any = this.binding?.get();
 			data.__origin = "";
 			return new Proxy(data, handler);
 		}
@@ -160,11 +160,16 @@ export default function Controller<T extends string>() {
 }
 
 /**
- * Sets property to element by selector within the container
- * @param selector HTML element selector
+ * Sets property to element by selector within the container.
+ * The default selector `.<property-name>` (class matches the property)
+ * is used when `@element`, `@element()`. Custom selector can be
+ * specified with `@element("<selector>")`.
+ * @param selector HTML query element selector
  */
-export function element(selector: string): Function {
-	return function(target: any, key: string): void {
+export function element(...args: any[]): any {
+	let selector: string | null = null;
+	const decorator = function(target: any, key: string): void {
+		selector = selector ? selector : `.${key}`;
 		const original = target.initialize;
 		target.initialize = function(...args: any[]): any {
 			Object.defineProperty(this, key, {
@@ -186,27 +191,166 @@ export function element(selector: string): Function {
 			return original.bind(this)(...args);
 		};
 	};
+
+	if (args.length == 0 || (args.length == 1 && typeof args[0] === "string")) {
+		selector = args.length ? args[0] : null;
+		return decorator;
+	} else if (args.length === 3) {
+		return decorator(args[0], args[1]);
+	}
 }
 
 /**
- * Sets property to all elements by selector within the container
- * @param selector HTML element selector
+ * Sets property to all elements by selector within the container.
+ * The default selector `.<property-name>` (class matches the property)
+ * is used when `@elements`, `@elements()`. Custom selector can be
+ * specified with `@elements("<selector>")`.
+ * @param selector HTML query element selector
  */
-export function elements(selector: string): Function {
-	return function(target: any, key: string): void {
+export function elements(...args: any[]): any {
+	let selector: string | null = null;
+	const decorator = function(target: any, key: string): void {
+		selector = selector ? selector : `.${key}`;
 		const original = target.initialize;
 		target.initialize = function(...args: any[]): any {
 			Object.defineProperty(this, key, {
 				get: () => {
-					return this.container.querySelectorAll(
-						selector
-					) as HTMLElement | null;
+					return [
+						...this.container.querySelectorAll(selector)
+					] as Node[];
 				}
 			});
 
 			return original.bind(this)(...args);
 		};
 	};
+
+	if (args.length == 0 || (args.length == 1 && typeof args[0] === "string")) {
+		selector = args.length ? args[0] : null;
+		return decorator;
+	} else if (args.length === 3) {
+		return decorator(args[0], args[1]);
+	}
+}
+
+/**
+ * Binds class property to data with a corresponding path.
+ * 2-way data binding maintains property's type while
+ * converting all its data to strings for DOM.
+ * Decorator called as `@bind`, `@bind()` binds with respect to property name.
+ * Custom bind path can be specified with `@bind("<path>")`.
+ * @param path Path to the data (same as `this.data.<path>`)
+ */
+export function bind(...args: any[]): any {
+	let path: string | null = null;
+	const decorator = function(target: any, key: string): void {
+		if (!path) path = key.toLowerCase();
+		let debounce = -1;
+		// eslint-disable-next-line @typescript-eslint/no-empty-function
+		let update: (name?: string, value?: any) => void = () => {};
+
+		//Proxy for objects handler
+		const handler: ProxyHandler<any> = {
+			get: function(target, prop, receiver) {
+				const result = Reflect.get(target, prop, receiver);
+				clearTimeout(debounce);
+				update();
+				return result;
+			},
+			set: function(target, prop, value) {
+				const result = Reflect.set(target, prop, value);
+				if (debounce == -1) {
+					update(`${path}.${prop.toString()}`, value);
+				}
+				return result;
+			}
+		};
+
+		let object = target[key];
+		let proxy =
+			typeof object == "object" ? new Proxy(object, handler) : object;
+
+		const getter = () => {
+			return proxy;
+		};
+		const setter = (value: any) => {
+			object = value;
+			if (typeof value == "object") {
+				proxy = new Proxy(object, handler);
+			} else {
+				proxy = object;
+			}
+			clearTimeout(debounce);
+			update();
+		};
+
+		//Redefine getter and setter for this property
+		Object.defineProperty(target, key, { get: getter, set: setter });
+
+		//Hook onto controller's initialize method to do more stuff
+		const original = target.initialize;
+		target.initialize = function(...args: any[]): any {
+			//Update hook
+			const setValue = (
+				name: string = path || key.toLowerCase(),
+				value: any = object
+			) => {
+				this.binding?.set(name, value);
+				debounce = -1;
+			};
+			update = (name?: string, value?: any) => {
+				debounce = setTimeout(() => {
+					setValue(name, value);
+				});
+			};
+			if (proxy == null && this.binding) {
+				setTimeout(() => {
+					//Update proxy value
+					object = this.binding?.get(path);
+					proxy =
+						typeof object == "object"
+							? new Proxy(object, handler)
+							: object;
+				});
+			} else {
+				setValue();
+			}
+
+			//Data-set hook
+			setTimeout(() => {
+				if (!this.binding) return;
+
+				const realSet = this.binding.set.bind(this.binding);
+				const hook = (name: string, value?: any, mode?: number) => {
+					//Hook only for the path
+					if (name === path) {
+						//Fetch value for default update
+						if (value === undefined) {
+							value = this.binding.get(path);
+						}
+
+						//Update proxy value
+						object = Utils.convertTo(object, value);
+						proxy =
+							typeof object == "object"
+								? new Proxy(object, handler)
+								: object;
+					}
+					return realSet(name, value, mode);
+				};
+				this.binding.set = hook;
+			});
+
+			return original.bind(this)(...args);
+		};
+	};
+
+	if (args.length == 0 || (args.length == 1 && typeof args[0] === "string")) {
+		path = args.length ? args[0]?.toLowerCase() : null;
+		return decorator;
+	} else if (args.length === 3) {
+		return decorator(args[0], args[1]);
+	}
 }
 
 if (typeof globalThis === "undefined") {

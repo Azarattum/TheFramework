f83f5a6e01b5ca06c7a731759e85bb5c37da0171
Azarattum <azarattum@pm.me>
2020-11-13 17:08:36 +0700

    Implemented expose component decorator

--- src/components/common/component.interface.ts
+++ src/components/common/component.interface.ts
@@ -41,3 +41,34 @@ export interface IComponentOptions {
 	/**Application refresh callback */
 	refresh: () => void;
 }
+
+/**
+ * Exposes function with `this.expose()` of current component.
+ * The default name (macthes method's name) is used when `@expose`, `@expose()`.
+ * Custom name can be specified with `@expose("<name>")`
+ * @param name Custom name for an exposed function
+ */
+export function expose(...args: any[]): any {
+	let name: string = "";
+	const decorator = function(
+		target: IComponent & { expose: (name: string, func: Function) => void },
+		key: string,
+		descriptor: PropertyDescriptor
+	): void {
+		name = name || key;
+		const original = target.initialize;
+		target.initialize = function(...args: any[]): any {
+			const func = descriptor.value?.bind(this);
+			if (func) this.expose?.(name, func);
+
+			return original?.bind(this)?.(...args);
+		};
+	};
+
+	if (args.length == 0 || (args.length == 1 && typeof args[0] === "string")) {
+		name = args.length ? args[0] : null;
+		return decorator;
+	} else if (args.length === 3) {
+		return decorator(args[0], args[1], args[2]);
+	}
+}
